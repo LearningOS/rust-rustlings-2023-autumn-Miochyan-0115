63728b641d16ba933db4576eb2c763cc114e39dc
oslab <1132370844@qq.com>
2023-10-15 22:33:17 +0800

    Finish

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,12 +4,11 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
+
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if let Some(value) = my_option{
-
-    }else{
         my_option.unwrap();
     }
 
@@ -25,7 +24,6 @@ fn main() {
 
     let mut value_a = 45;
     let mut value_b = 66;
-    // Let's swap these two!
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
